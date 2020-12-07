b8cd4d7c40883cb2d3e522facaa76dcae6d83b76
Edison Li <edison4354@gmail.com>
2020-12-07 11:40:36 -0800

    Background images and sections

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.header').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.model-s').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.model-3').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.model-x').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.model-y').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.solar-roof').height($(window).height());
+})
+
+$(document).ready(function(){
+	$('.accessories').height($(window).height());
+})
